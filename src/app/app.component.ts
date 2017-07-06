215b6fdf83076617960365fcf2da6eb53be149b7
Remus Lazar <rl@cron.eu>
2017-07-06 18:59:41 +0200

    Date Formatter tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import {Component, Inject, LOCALE_ID} from '@angular/core';
 export class AppComponent {
 
   number = 123.23;
+  date = new Date();
 
   constructor(@Inject(LOCALE_ID) public localeID: string)  {}
 }
